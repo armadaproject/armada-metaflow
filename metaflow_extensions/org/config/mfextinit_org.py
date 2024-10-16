93e2c4e7b2b5837477e4648785fcac19862e65d6
Clif Houck <me@clifhouck.com>
2024-10-16 09:56:08 -0500

    Add some default configuration for armada-metaflow
    Signed-off-by: Clif Houck <me@clifhouck.com>

--- metaflow_extensions/org/config/mfextinit_org.py
+++ metaflow_extensions/org/config/mfextinit_org.py
@@ -1,4 +1,5 @@
-###
+"""Configuration for armada-metaflow extension."""
+
 # CONFIGURE: Your company specific configuration options can go in this file.
 #            Any settings present here will override the ones present in
 #            metaflow_config.py. You can also optionally add additional configuration
@@ -9,21 +10,19 @@
 
 from metaflow.metaflow_config_funcs import from_conf
 
-# EXAMPLE: Force a specific datastore
+# Armada currently supports S3.
 DEFAULT_DATASTORE = "s3"
-
-# EXAMPLE: Configure the bucket to use
-DATASTORE_SYSROOT_S3 = "s3://mycompany/mymetaflowbucket"
+DATASTORE_SYSROOT_S3 = "s3://g-research/armada-metaflow"
 
 # EXAMPLE: use from_conf to correctly allow values from the environment/configuration file
-
-DEFAULT_METADATA = from_conf("DEFAULT_METADATA", "mymetadata") 
+# DEFAULT_METADATA = from_conf("DEFAULT_METADATA", "mymetadata")
 
 ###
 # CONFIGURE: You can also specify additional debugging options that are
 #            activated using the METAFLOW_DEBUG_XXX environment variable. See debug.py
 ###
-DEBUG_OPTIONS = ["myspecialdebug"]
+# DEBUG_OPTIONS = ["myspecialdebug"]
+
 
 ###
 # CONFIGURE: You can override any conda dependencies when a Conda environment is created
@@ -31,6 +30,6 @@ DEBUG_OPTIONS = ["myspecialdebug"]
 def get_pinned_conda_libs(python_version, datastore_type):
     return {
         "click>=8.0.0",
-        "requests=2.24.0"
+        "requests=2.24.0",
         # ...
     }
